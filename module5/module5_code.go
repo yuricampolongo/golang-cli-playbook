3877cd3c1426ec003e891640b0926cc16b4ca8ea
Yuri Campolongo <yuricampolongo@outlook.com>
2021-04-27 21:51:09 -0400

    - site error

--- module5/module5_code.go
+++ module5/module5_code.go
@@ -1,15 +1,12 @@
 package module5
 
-import (
-	"fmt"
-	"net/http"
-)
-
+// GetExampleDotCom uses the "net/http" package to send a GET request to example.com
+/*
 func GetExampleDotCom() {
 	resp, err := http.Get("http://example.com/")
 	if err != nil {
 		fmt.Println("something went wrong")
 	}
-
 	defer resp.Body.Close()
 }
+*/
